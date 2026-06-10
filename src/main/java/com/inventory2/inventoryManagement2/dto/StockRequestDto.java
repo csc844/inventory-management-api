dbe659be03c09dba0c88cad3210b2d75a87783f4
csc844 <ansh844csc@gmail.com>
2026-06-10 14:37:10 +0530

    validation

--- src/main/java/com/inventory2/inventoryManagement2/dto/StockRequestDto.java
+++ src/main/java/com/inventory2/inventoryManagement2/dto/StockRequestDto.java
@@ -1,13 +1,15 @@
 package com.inventory2.inventoryManagement2.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
-
 @Data
 public class StockRequestDto {
 
+    @NotNull(message = "Product Id is required")
     private Long productId;
 
+    @NotNull(message = "Quantity is required")
+    @Positive(message = "Quantity must be greater than zero")
     private Integer quantity;
-
-    private Integer minimumLevel;
 }
